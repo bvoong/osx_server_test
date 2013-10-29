a9c42e5de6b48cdece3bd495387150020e46a81a
Brian <bvoong@appdynamics.com>
2013-10-29 11:41:26 -0700

    init

---
+++ testOSXServer/testOSXServer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testOSXServer
+//
+//  Created by Brian Voong on 10/29/13.
+//  Copyright (c) 2013 Brian Voong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testOSXServer/testOSXServerTests/testOSXServerTests.m
@@ -0,0 +1,34 @@
+//
+//  testOSXServerTests.m
+//  testOSXServerTests
+//
+//  Created by Brian Voong on 10/29/13.
+//  Copyright (c) 2013 Brian Voong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testOSXServerTests : XCTestCase
+
+@end
+
+@implementation testOSXServerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
