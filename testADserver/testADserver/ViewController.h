5f793ce6d9ca11f5b4061ebd2a32d14f04669fef
Brian <bvoong@appdynamics.com>
2013-11-11 12:33:20 -0800

    123

---
+++ testADserver/testADserver/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testADserver
+//
+//  Created by Brian Voong on 11/11/13.
+//  Copyright (c) 2013 Brian Voong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ testADserver/testADserverTests/testADserverTests.m
@@ -0,0 +1,34 @@
+//
+//  testADserverTests.m
+//  testADserverTests
+//
+//  Created by Brian Voong on 11/11/13.
+//  Copyright (c) 2013 Brian Voong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testADserverTests : XCTestCase
+
+@end
+
+@implementation testADserverTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
